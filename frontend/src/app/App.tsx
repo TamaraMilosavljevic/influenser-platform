69268dba7875cb51f4b861e65c0924967b7e4445
DusanBrankovic <dbrankovic3819rn@raf.rs>
2025-11-29 12:41:01 +0100

    Test push 2

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br  from-fuchsia-700 via-purple-300 to-violet-600">
       <div className="flex flex-row rounded-xl bg-white/30 px-6 py-4 shadow-lg">
         <p className="text-2xl mt-2 font-medium text-slate-600/70">
-          Hello World loading... (test push)
+          Hello World loading... (test push 2)
         </p>
         <div className="w-20 h-20">
           <img src="tempicon.png" />
