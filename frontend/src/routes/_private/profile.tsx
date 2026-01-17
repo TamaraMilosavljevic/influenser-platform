4a7600b4d13c47f601a7cfe1990fe5fa68d345bb
DusanBrankovic <dbrankovic3819rn@raf.rs>
2026-01-17 03:04:55 +0100

    Added initial data fetch on profile route

--- frontend/src/routes/_private/profile.tsx
+++ frontend/src/routes/_private/profile.tsx
@@ -1,6 +1,25 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import ProfilePage from "@/pages/ProfilePage";
+import { getLoggedInInfluencer } from "@/services/influencerService";
+import { getAccessTokenData } from "@/auth/authStore";
 
 export const Route = createFileRoute("/_private/profile")({
+  beforeLoad: async () => {
+
+    const userId = getAccessTokenData()?.sub;
+
+    console.log("Fetching influencer profile for userId:", userId);
+
+    const influencer = await getLoggedInInfluencer(userId!);
+
+    if (!influencer) {
+      throw redirect({ to: "/auth" });
+    }
+
+    return {
+      influencer,
+    };
+  },
+
   component: ProfilePage,
 });
